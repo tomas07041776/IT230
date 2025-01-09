179bb74e980c229465707db0b9ee444cb55e66da
Thomas Noel <thomas.noel@snhu.edu>
2025-01-09 12:18:57 -0500

    Add project files.

---
+++ HelloYourName/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿//Thomas Noel
+//IT-230 Dr. Wetsch
+//Module 1-4 Coding Activity: Your First C# WPF Application
+//HelloYourName
+
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloYourName
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void okClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Hello {userName.Text}");
+        }
+    }
+}
